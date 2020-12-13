0ab5a8c6b215e0b135161753fe91850ce6c93e30
youbin-shin <youbin5575@gmail.com>
2020-12-13 22:40:24 +0900

    mypage 수정

--- front/src/api.js
+++ front/src/api.js
@@ -1,7 +1,8 @@
 export default {
     URL: "http://k3a301.p.ssafy.io:8399",
     ROUTES: {
-      sprint: "/api/sprint//",
+      sprint: "/api/sprint/",
+      mypage: "/api/marathon/completeList"
     },
   };
   
